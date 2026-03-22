6647ca03b8ff1cae38b4b36fb1f164ca4336ef9f
scottnguy3n101 <scottnbusiness101@gmail.com>
2026-03-22 22:32:35 +1100

    Added comments to the basic UI elements

--- prototypes/ScottNguyen_Prototype/src/Frontend/GameFrame.java
+++ prototypes/ScottNguyen_Prototype/src/Frontend/GameFrame.java
@@ -2,18 +2,29 @@ package Frontend;
 
 import javax.swing.*;
 
+/**
+ * Class that represents the main game window to be used for Ticket To Ride
+ */
 public class GameFrame{
     private JFrame frame;
 
+    /**
+     * Constructor for GameFrame class. Creates a new JFrame.
+     */
     public GameFrame() {
         frame = new JFrame();
     }
 
+    /**
+     * Starts the UI, sets GameFrame attributes. Adds GameScreenManage to the frame.
+     */
     public void startUI() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setTitle("Ticket To Ride");
+
+        frame.add(new GameScreenManager());
     }
 }
--- prototypes/ScottNguyen_Prototype/src/Frontend/GameScreenManager.java
+++ prototypes/ScottNguyen_Prototype/src/Frontend/GameScreenManager.java
@@ -3,10 +3,17 @@ package Frontend;
 import javax.swing.JPanel;
 import java.awt.*;
 
+/**
+ * Class that manages the content shown on the GameFrame. Uses a CardLayout to
+ * switch between cards (screens) in the game.
+ */
 public class GameScreenManager extends JPanel {
 
     private CardLayout layout;
 
+    /**
+     * Constructor for the GameScreenManager(), which manages the screens of the game
+     */
     public GameScreenManager() {
         layout = new CardLayout();
 
@@ -14,6 +21,11 @@ public class GameScreenManager extends JPanel {
 
     }
 
+    /**
+     * Used to display a specified screen
+     *
+     * @param name the name of the screen to be shown on the frame
+     */
     public void showScreen(String name) {
         layout.show(this, name);
     }
